c307134e6a0225f58bd205722d86f7f1931d0663
Pratap Gajjala <pratap@greyminds.com>
2018-06-13 16:02:01 -0400

    more project directory structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedTestComponent } from './shared/components/shared-test/shared-test.component';
+import { SharedPipePipe } from './shared/pipes/shared-pipe.pipe';
+import { SharedDirectiveDirective } from './shared/directives/shared-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedTestComponent,
+    SharedPipePipe,
+    SharedDirectiveDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/shared-test/shared-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fox-shared-test',
+  templateUrl: './shared-test.component.html',
+  styleUrls: ['./shared-test.component.scss']
+})
+export class SharedTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
