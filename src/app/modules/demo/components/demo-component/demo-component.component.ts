cb6a5542844404fa46775adb4b3b27a41b0bdfb1
Pratap Gajjala <pratap@greyminds.com>
2018-06-14 15:15:47 -0400

    directory structure stuff

---
+++ src/app/modules/demo/components/demo-component/demo-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fox-demo-component',
+  templateUrl: './demo-component.component.html',
+  styleUrls: ['./demo-component.component.scss']
+})
+export class DemoComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/demo/demo.module.ts
+++ src/app/modules/demo/demo.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestComponent } from './test/test.component';
+import { DemoComponentComponent } from './components/demo-component/demo-component.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TestComponent]
+  declarations: [TestComponent, DemoComponentComponent]
 })
 export class DemoModule { }
