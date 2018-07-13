c520ec42882d8977401df1ed0ac07b76104c1b36
Pratap Gajjala <pratap@greyminds.com>
2018-07-13 11:33:45 -0400

    adding some ngxs dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedTestComponent } from './shared/components/shared-test/shared-test.component';
 import { SharedPipePipe } from './shared/pipes/shared-pipe.pipe';
 import { SharedDirectiveDirective } from './shared/directives/shared-directive.directive';
+import { RouterState } from './shared/router.state';
+import { AppState } from './shared/app.state';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,13 @@ import { SharedDirectiveDirective } from './shared/directives/shared-directive.d
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsFormPluginModule.forRoot(),
+    NgxsModule.forRoot([
+      RouterState,
+      AppState
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
