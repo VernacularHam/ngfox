2ad4bdde5623329f69aa17d6f6d4c20d01b9c438
Pratap Gajjala <pratap@greyminds.com>
2018-08-01 14:53:02 -0400

    fixing app component unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,26 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Shallow } from 'shallow-render';
-
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
 
+// Not using shallow-render here because AppComponent is a weird case. Shouldn't need those capabilities here too badly.
 describe('AppComponent', () => {
-  let shallow: Shallow<AppComponent>;
-
-  beforeEach(() => {
-    shallow = new Shallow(AppComponent, AppModule);
-  });
-
-  it('should create the app', async () => {
-    // const fixture = TestBed.createComponent(AppComponent);
-    // const app = fixture.debugElement.componentInstance;
-    // expect(app).toBeTruthy();
-
-    const {find} = await shallow.render('<fox-root></fox-root>');
-
-    expect(find('a').nativeElement.innerText).toBeTruthy();
-  });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+      schemas: [
+        // This tells the unit test compiler to ignore unrecognized elements
+        // and directives and removes the need to mock them when unit testing a 'smart' component.
+        NO_ERRORS_SCHEMA
+      ]
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
   it(`should have as title 'fox'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -30,6 +34,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to fox!');
+    expect(compiled.querySelector('h1').textContent).toContain('fox UI Prototype');
   }));
 });
